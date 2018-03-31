72acd0b966b5c89df801ea321ccec9871a33198a
“Zhangsl <1083065285@qq.com>
2018-03-31 18:55:52 +0800

    代码修改 添加 增删改功能

--- src/components/page/ProductManager.vue
+++ src/components/page/ProductManager.vue
@@ -9,7 +9,7 @@
         <div class="handle-box">
             <el-button type="primary" icon="delete" class="handle-del mr10" @click="delAll">批量删除</el-button>
             <el-button type="primary" icon="add" class="handle-del mr10" @click="addData">添加产品</el-button>
-            <el-input v-model="select_word" placeholder="筛选关键词" class="handle-input mr10"></el-input>
+            <!--<el-input v-model="select_word" placeholder="筛选关键词" class="handle-input mr10"></el-input>-->
             <el-button type="primary" icon="search" @click="search">搜索</el-button>
         </div>
         <el-table :data="tableDataEnd" border style="width: 100%" ref="multipleTable" @selection-change="handleSelectionChange">
@@ -55,12 +55,12 @@
                     <el-input v-model="dialogEditForm.alias" auto-complete="off"></el-input>
                 </el-form-item>
                 <el-form-item label="产品描述:" :label-width="formLabelWidth">
-                    <el-input v-model="dialogEditForm.des" auto-complete="off"></el-input>
+                    <el-input v-model="dialogEditForm.desc" auto-complete="off"></el-input>
                 </el-form-item>
             </el-form>
             <div slot="footer" class="dialog-footer">
                 <el-button @click="dialogFormEditVisible = false">重 置</el-button>
-                <el-button type="primary" @click="dialogFormEditVisible = false">提 交</el-button>
+                <el-button type="primary" @click="handleEditForm">提 交</el-button>
             </div>
         </el-dialog>
         <el-dialog title="产品添加" :visible.sync="dialogFormAddVisible">
@@ -72,7 +72,7 @@
                     <el-input v-model="dialogAddForm.alias" auto-complete="off"></el-input>
                 </el-form-item>
                 <el-form-item label="产品描述:" :label-width="formLabelWidth">
-                    <el-input v-model="dialogAddForm.des" auto-complete="off"></el-input>
+                    <el-input v-model="dialogAddForm.desc" auto-complete="off"></el-input>
                 </el-form-item>
                 <el-form-item label="产品类别" :label-width="formLabelWidth">
                 <el-select v-model="dialogAddForm.type" placeholder="请选择产品类别">
@@ -87,7 +87,7 @@
             </el-form>
             <div slot="footer" class="dialog-footer">
                 <el-button @click="dialogFormAddVisible = false">重 置</el-button>
-                <el-button type="primary" @click="dialogFormAddVisible = false">提 交</el-button>
+                <el-button type="primary" @click="addDataForm">提 交</el-button>
             </div>
         </el-dialog>
         <el-dialog
@@ -98,7 +98,7 @@
             <span>确认删除？</span>
             <span slot="footer" class="dialog-footer">
                 <el-button @click="dialogVisible = false">取 消</el-button>
-                <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+                <el-button type="primary" @click="deleteForm">确 定</el-button>
          </span>
         </el-dialog>
     </div>
@@ -107,6 +107,7 @@
 <script>
 
     import axios from 'axios';
+    import Qs from 'qs'
     export default {
         data() {
             return {
@@ -133,6 +134,9 @@
                     alias: '',
                     sncode: ''
                 },
+                dialogDeleteForm:{
+                    sncode: ''
+                },
                 flag:false
             }
         },
@@ -148,11 +152,15 @@
                         obj.sncode = resultData[i].sncode;
                         obj.alias = resultData[i].alias;
                         obj.des = resultData[i].des;
-                        if (resultData[i].state == '0'){
+                        if (resultData[i].bind == '0'){
                             obj.state = '未绑定';
+                        }else if (resultData[i].bind == '1'){
+                            obj.state = '已绑定';
                         }
                         if (resultData[i].online == '0'){
                             obj.online = '未运行';
+                        }else if (resultData[i].online == '1'){
+                            obj.online = '已运行';
                         }
                         obj.addtimeString = resultData[i].addtimeString;
                         tableData.push(obj);
@@ -169,7 +177,6 @@
                 }).catch(function (error) {
                 console.log(error);
             });
-
         },
         methods: {
             //前端搜索功能需要区分是否检索,因为对应的字段的索引不同
@@ -235,10 +242,142 @@
             handleEdit(index, row) {
 //                this.$message('编辑第'+(index+1)+'行');
                 this.dialogFormEditVisible = true;
+                var data = this.tableDataEnd[index];
+                var sncode = data.sncode;
+                var desc = data.des;
+                var  alias = data.alias;
+                this.dialogEditForm.sncode = sncode;
+                this.dialogEditForm.alias = alias;
+                this.dialogEditForm.desc = desc;
+            },
+            handleEditForm(){
+                var sncode = this.dialogEditForm.sncode;
+                var desc = this.dialogEditForm.desc;
+                var  alias = this.dialogEditForm.alias;
+                var form_data= Qs.stringify(this.dialogEditForm);
+                this.dialogFormEditVisible = false;
+                this.$axios({
+                    method:'post',
+                    url:'http://localhost:8080/editDevice',
+                    responseType:'json',
+                    data:form_data,
+                    headers: {
+                            'Content-Type': 'application/x-www-form-urlencoded'
+                        }
+                    }).then(function(response) {
+                        var jsonObject = response.data;
+                        var  resultData = jsonObject.data;
+                        if (jsonObject.code == 0){
+                            console.log(jsonObject.msg);
+                        }
+                    })
+                    .catch(function (error) {
+                        console.log("发生错误了");
+                        this.dialogFormEditVisible = false;
+                    });
+                var tableData = [];
+                let _this = this;
+                this.$axios.get('http://localhost:8080/getProductVue')
+                    .then(function (response) {
+                        var jsonObject = response.data;
+                        var  resultData = jsonObject.data;
+                        for (var i = 0;i < resultData.length;i++){
+                            var  obj = {};
+                            obj.sncode = resultData[i].sncode;
+                            obj.alias = resultData[i].alias;
+                            obj.des = resultData[i].des;
+                            if (resultData[i].bind == '0'){
+                                obj.state = '未绑定';
+                            }else if (resultData[i].bind == '1'){
+                                obj.state = '已绑定';
+                            }
+                            if (resultData[i].online == '0'){
+                                obj.online = '未运行';
+                            }else if (resultData[i].online == '1'){
+                                obj.online = '已运行';
+                            }
+                            obj.addtimeString = resultData[i].addtimeString;
+                            tableData.push(obj);
+                        }
+                        _this.tableDataBegin = tableData;
+                        _this.totalItems = _this.tableDataBegin.length;
+                        if (_this.totalItems > _this.pageSize) {
+                            for (let index = 0; index < _this.pageSize; index++) {
+                                _this.tableDataEnd.push(_this.tableDataBegin[index]);
+                            }
+                        } else {
+                            _this.tableDataEnd = _this.tableDataBegin;
+                        }
+                    }).catch(function (error) {
+                    console.log(error);
+                });
             },
             handleDelete(index, row) {
 //                this.$message.error('删除第'+(index+1)+'行');
+                var data = this.tableDataEnd[index];
+                var sncode = data.sncode;
+                this.dialogDeleteForm.sncode = sncode;
                 this.dialogVisible = true;
+            },
+            deleteForm(){
+                this.dialogVisible = false;
+                var form_data= Qs.stringify(this.dialogDeleteForm);
+                this.$axios({
+                    method:'post',
+                    url:'http://localhost:8080/deleteProductListVue',
+                    responseType:'json',
+                    data:form_data,
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    }
+                }).then(function(response) {
+                    var jsonObject = response.data;
+                    var  resultData = jsonObject.data;
+                    if (jsonObject.code == 0){
+                        console.log(jsonObject.msg);
+                    }
+                })
+                    .catch(function (error) {
+                        console.log("发生错误了");
+                        this.dialogFormEditVisible = false;
+                    });
+
+                var tableData = [];
+                let _this = this;
+                this.$axios.get('http://localhost:8080/getProductVue')
+                    .then(function (response) {
+                        var jsonObject = response.data;
+                        var  resultData = jsonObject.data;
+                        for (var i = 0;i < resultData.length;i++){
+                            var  obj = {};
+                            obj.sncode = resultData[i].sncode;
+                            obj.alias = resultData[i].alias;
+                            obj.des = resultData[i].des;
+                            if (resultData[i].bind == '0'){
+                                obj.state = '未绑定';
+                            }else if (resultData[i].bind == '1'){
+                                obj.state = '已绑定';
+                            }
+                            if (resultData[i].online == '0'){
+                                obj.online = '未运行';
+                            }else if (resultData[i].online == '1'){
+                                obj.online = '已运行';
+                            }
+                            obj.addtimeString = resultData[i].addtimeString;
+                            tableData.push(obj);
+                        }
+                        _this.tableDataBegin = tableData;
+                        _this.totalItems = _this.tableDataBegin.length;
+                        if (_this.totalItems > _this.pageSize) {
+                            for (let index = 0; index < _this.pageSize; index++) {
+                                _this.tableDataEnd.push(_this.tableDataBegin[index]);
+                            }
+                        } else {
+                            _this.tableDataEnd = _this.tableDataBegin;
+                        }
+                    }).catch(function (error) {
+                    console.log(error);
+                });
             },
             delAll(){
 //                const self = this,
@@ -254,6 +393,75 @@
             },
             addData(){
                 this.dialogFormAddVisible = true;
+            },
+            addDataForm(){
+                var alias =  this.dialogAddForm.alias;
+                var desc = this.dialogAddForm.desc;
+                var sncode = this.dialogAddForm.sncode;
+                var  type = this.dialogAddForm.type[0];
+                var data = Qs.stringify({
+                    "alias":alias,
+                    "desc":desc,
+                    "sncode":sncode,
+                    "type":type
+                });
+                this.dialogFormAddVisible = false;
+                this.$axios({
+                    method:'post',
+                    url:'http://localhost:8080/addDevice',
+                    responseType:'json',
+                    data:data,
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    }
+                }).then(function(response) {
+                        var jsonObject = response.data;
+                        var  resultData = jsonObject.data;
+                        if (jsonObject.code == 0){
+                            console.log(jsonObject.msg);
+                        }
+                    })
+                    .catch(function (error) {
+                        console.log("发生错误了");
+                        this.dialogFormAddVisible = false;
+                    });
+
+                var tableData = [];
+                let _this = this;
+                this.$axios.get('http://localhost:8080/getProductVue')
+                    .then(function (response) {
+                        var jsonObject = response.data;
+                        var  resultData = jsonObject.data;
+                        for (var i = 0;i < resultData.length;i++){
+                            var  obj = {};
+                            obj.sncode = resultData[i].sncode;
+                            obj.alias = resultData[i].alias;
+                            obj.des = resultData[i].des;
+                            if (resultData[i].bind == '0'){
+                                obj.state = '未绑定';
+                            }else if (resultData[i].bind == '1'){
+                                obj.state = '已绑定';
+                            }
+                            if (resultData[i].online == '0'){
+                                obj.online = '未运行';
+                            }else if (resultData[i].online == '1'){
+                                obj.online = '已运行';
+                            }
+                            obj.addtimeString = resultData[i].addtimeString;
+                            tableData.push(obj);
+                        }
+                        _this.tableDataBegin = tableData;
+                        _this.totalItems = _this.tableDataBegin.length;
+                        if (_this.totalItems > _this.pageSize) {
+                            for (let index = 0; index < _this.pageSize; index++) {
+                                _this.tableDataEnd.push(_this.tableDataBegin[index]);
+                            }
+                        } else {
+                            _this.tableDataEnd = _this.tableDataBegin;
+                        }
+                    }).catch(function (error) {
+                    console.log(error);
+                });
             },
             handleSelectionChange(val) {
                 this.multipleSelection = val;
